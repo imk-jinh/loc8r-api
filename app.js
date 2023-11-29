23c7c6231441df6ed550d17bcf31b6d1b2cb699f
Jinhyung Kim <123533660+jinhyungkim980@users.noreply.github.com>
2023-11-30 01:14:26 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
 require('./app_api/models/db');
+require('./app_api/config/passport');
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -18,15 +21,51 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
+app.use(passport.initialize());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+
+// app.get('*', function (req, res, next) {
+//   res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+// });
+
+app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+});
+
+app.use('/api', (req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:4200);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, \
+  Content-Type, Accept, Authorization'
+  );
+  next();
+});
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ message: err.name + ': ' + err.message });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
